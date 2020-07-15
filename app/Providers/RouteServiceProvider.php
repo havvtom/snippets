911c79a6b9bd033314657e2507fad012d2e511fa
havvtom <havvtom@gmail.com>
2020-07-15 15:59:23 +0200

    routes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -30,7 +30,7 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Route::pattern('post', '[0-9]+');
 
         parent::boot();
     }
